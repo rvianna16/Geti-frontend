24a8db77cc1501bc2c8bd44c54f9bfc98b0c4169
Rodrigo Vianna <roodrigo.vs123@hotmail.com>
2022-05-08 16:05:17 -0300

    update envirtoment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'https://localhost:5001/api/',
+  apiUrl: 'https://getiapi20220508144721.azurewebsites.net/api/',
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  apiUrl: 'https://getiapi20220508144721.azurewebsites.net/api/',
+  apiUrl: 'https://localhost:5001/api/',
   production: false
 };
 
