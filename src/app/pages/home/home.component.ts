d038f70c26aea6067982860a30f6fab7e5ec4a7c
Rodrigo Vianna <roodrigo.vs123@hotmail.com>
2022-05-08 19:21:48 -0300

    feat - redirecionando página contagem

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Contagens } from './models/contagens';
 import { ContagensService } from './services/contagens.service';
 
@@ -15,9 +16,28 @@ export class HomeComponent implements OnInit {
     softwares: 0,
   }
 
-  constructor(private contagensService: ContagensService) { }
+  constructor(
+    private contagensService: ContagensService,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
     this.contagensService.obterContagens().subscribe(res => this.contagens = res);
+  }
+
+  visualizarColaboradores(){
+    this.router.navigate(['colaboradores']);
+  }
+
+  visualizarEquipamentos(){
+    this.router.navigate(['equipamentos']);
+  }
+
+  visualizarLicencas(){
+    this.router.navigate(['licencas']);
+  }
+
+  visualizarSoftwares(){
+    this.router.navigate(['softwares']);
   }
 }
